31f473a1d9d2bbf63efe97910d9bda0c08db5ce3
Maksim Klyuev <maks.klyuev.2000@mail.ru>
2020-11-29 15:15:31 +0300

    spring security commit 3 coment with added files

--- src/main/java/com/nayar/demo/controller/InfoController.java
+++ src/main/java/com/nayar/demo/controller/InfoController.java
@@ -1,4 +1,16 @@
 package com.nayar.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class InfoController {
+
+    @GetMapping("/info")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "Info";
+    }
+
 }
